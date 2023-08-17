8f9e868bd3a918acb1810b0369b0833280002a85
hilleld <hillel.darshan@mail.huji.ac.il>
2023-08-17 15:41:22 +0300

    volcano plot version1 - passed

--- game1.py
+++ game1.py
@@ -1,6 +1,7 @@
-# Hillel Darshan 8/8/23
+# Hillel Darshan 8/17/23
 import numpy as np
 import pandas as pd
+import plotly.graph_objects as go
 
 
 
@@ -31,13 +32,35 @@ def load_data(filename: str) -> pd.DataFrame:
 
 
 if __name__ == '__main__':
-    df = load_data("./small_data_preproccesed.csv")
-    sources_list = df["tRNA Source"].unique()
-    sources_indexes = np.zeros(len(sources_list))
-    sources_indexes = np.vstack((sources_list, sources_indexes)).T
-    data = df.to_numpy()
-    for source in sources_list:
-        pass
+    # data_files = ["C:/Users/H/PycharmProjects/ALS_analysis/working data - ALS/GSE203170_tRF_SgGens_output.csv"]
+    data_files = ["../ALS_analysis/working data - ALS/GSE203170_tRF_SgGens_output.csv"]
+    df = pd.read_csv(data_files[0]) # todo: make it work for all data sets
+
+    # sources_list = df["transcript"].unique()
+
+    # refactor logFC from ln to log2
+    df["logFC"] = np.log2(np.exp(df["logFC"]))
+    # refactor FDR to -log10 FDR
+    df["FDR"] = -np.log10(df["FDR"])
+
+    pl = go.Figure(go.Scatter(x=df["logFC"],
+                              y=df["FDR"],
+                              mode="markers",
+                              marker=dict(color="blue")))
+    pl.update_layout(title="ALS " + "SOMTHING",
+                     xaxis_title="Log2 ????",
+                     yaxis_title="Log(-10) ????")
+
+    pl.show()
+
+
+
+
+    # sources_indexes = np.zeros(len(sources_list))
+    # sources_indexes = np.vstack((sources_list, sources_indexes)).T
+    # data = df.to_numpy()
+    # for source in sources_list:
+    #     pass
 
 
 
