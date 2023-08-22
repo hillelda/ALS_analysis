75455b8796e646a55804cc13aa77553fa0eefcbc
hilleld <hillel.darshan@mail.huji.ac.il>
2023-08-22 17:59:47 +0300

    added meta_data to output

--- ALS_BoNT_analysis.py
+++ ALS_BoNT_analysis.py
@@ -9,8 +9,8 @@ def draw_volcano_plots(df_list, full_add_list, ds_name_list):
         df['binary PValue color'] = df['FDR'].apply(lambda x: "red" if x > -np.log10(0.05) else "blue")
 
         pl = go.Figure(
-            go.Scatter(x=df["logFC"],
-                       y=df["FDR"],
+            go.Scatter(x=df["log2 FC"],
+                       y=df["-log 10 FDR"],
                        mode="markers",
                        marker=dict(color=df['binary PValue color'])))
         pl.update_layout(title="ALS " + ds_name_list[i],
@@ -29,15 +29,15 @@ def pre_process_als(files_location, data_files, df_list, full_add_list, ds_name_
         ds_name_list.append(split_name[0] + " " + split_name[1])
 
         # refactor logFC from ln to log2
-        df["logFC"] = np.log2(np.exp(df["logFC"]))
+        df["log2 FC"] = np.log2(np.exp(df["logFC"]))
         # refactor FDR to -log10 FDR
-        df["FDR"] = -np.log10(df["FDR"])
+        df["-log 10 FDR"] = -np.log10(df["FDR"])
 
         # Add one-hot columns for significance and sign
-        df['sig up als'] = (df['FDR'] < 0.05) & (df['logFC'] >= 0)
-        df['sig down als'] = (df['FDR'] < 0.05) & (df['logFC'] < 0)
-        df['unsig up als'] = (df['FDR'] >= 0.05) & (df['logFC'] >= 0)
-        df['unsig down als'] = (df['FDR'] >= 0.05) & (df['logFC'] < 0)
+        df['sig up als'] = (df['-log 10 FDR'] < 0.05) & (df['logFC'] >= 0)
+        df['sig down als'] = (df['-log 10 FDR'] < 0.05) & (df['logFC'] < 0)
+        df['unsig up als'] = (df['-log 10 FDR'] >= 0.05) & (df['logFC'] >= 0)
+        df['unsig down als'] = (df['-log 10 FDR'] >= 0.05) & (df['logFC'] < 0)
 
         df.set_index('transcript', inplace=True)
         df_list.append(df)
@@ -47,14 +47,15 @@ def pre_process_BoNT(files_location_address, cur_file):
     full_add = files_location_address + cur_file
     Bo_NT_df = pd.read_csv(full_add)
     # refactor logFC from ln to log2
-    Bo_NT_df["logFC"] = np.log2(np.exp(Bo_NT_df["logFC"]))
+    Bo_NT_df["log2 FC"] = np.log2(np.exp(Bo_NT_df["logFC"]))
     # refactor FDR to -log10 FDR
-    Bo_NT_df["FDR"] = -np.log10(Bo_NT_df["FDR"])
+    Bo_NT_df["-log 10 FDR"] = -np.log10(Bo_NT_df["FDR"])
+
     # Add one-hot columns for significance and sign
-    Bo_NT_df['sig up bont'] = (Bo_NT_df['FDR'] < 0.05) & (Bo_NT_df['logFC'] >= 0)
-    Bo_NT_df['sig down bont'] = (Bo_NT_df['FDR'] < 0.05) & (Bo_NT_df['logFC'] < 0)
-    Bo_NT_df['unsig up bont'] = (Bo_NT_df['FDR'] >= 0.05) & (Bo_NT_df['logFC'] >= 0)
-    Bo_NT_df['unsig down bont'] = (Bo_NT_df['FDR'] >= 0.05) & (Bo_NT_df['logFC'] < 0)
+    Bo_NT_df['sig up bont'] = (Bo_NT_df['-log 10 FDR'] < 0.05) & (Bo_NT_df['logFC'] >= 0)
+    Bo_NT_df['sig down bont'] = (Bo_NT_df['-log 10 FDR'] < 0.05) & (Bo_NT_df['logFC'] < 0)
+    Bo_NT_df['unsig up bont'] = (Bo_NT_df['-log 10 FDR'] >= 0.05) & (Bo_NT_df['logFC'] >= 0)
+    Bo_NT_df['unsig down bont'] = (Bo_NT_df['-log 10 FDR'] >= 0.05) & (Bo_NT_df['logFC'] < 0)
 
     Bo_NT_df.drop(Bo_NT_df.columns[0], axis=1, inplace=True)
     Bo_NT_df.set_index('transcript', inplace=True)
@@ -71,15 +72,18 @@ def cross_sig_dfs(combined_df):
 
 
 if __name__ == '__main__':
-    files_location = "../ALS_analysis/working data - ALS/"
+    working_dir = "../ALS_analysis/working data - ALS/"
     data_files = ["GSE203170_miRNA_SgGens_output.csv", "GSE203170_tRF_SgGens_output.csv", "GSE94888_miRNA_SgGens_output.csv", "GSE94888_tRF_SgGens_output.csv"]
     df_list = []
     full_add_list = []
     ds_name_list = []
-    pre_process_als(files_location, data_files, df_list, full_add_list, ds_name_list)
+    pre_process_als(working_dir, data_files, df_list, full_add_list, ds_name_list)
     draw_volcano_plots(df_list, full_add_list, ds_name_list)
 
-    bont_pd = pre_process_BoNT(files_location, "DE_tRFs Arik BoNT_A 16.8.23.csv")
+    bont_pd = pre_process_BoNT(working_dir, "DE_tRFs Arik BoNT_A 16.8.23.csv")
+
+    meta_203170 = pd.read_csv(working_dir + 'GSE203170_tRF_meta.csv')
+    meta_94888 = pd.read_csv(working_dir + 'GSE94888_tRF_meta.csv')
 
     combined_df_1 = pd.concat([df_list[1], bont_pd], axis=1, join='outer', sort=False)
     combined_df_2 = pd.concat([df_list[3], bont_pd], axis=1, join='outer', sort=False)
@@ -87,13 +91,17 @@ if __name__ == '__main__':
     combined_df_1 = cross_sig_dfs(combined_df_1)
     combined_df_2 = cross_sig_dfs(combined_df_2)
 
-    # column_to_drop = ['sig up als', 'sig down als', 'unsig up als', 'unsig down als', 'sig up bont', 'sig down bont',
-    #                   'unsig up bont', 'unsig down bont', 'isSg']
-    #
-    # combined_df_1.drop(column_to_drop, axis=1, inplace=True)
+    # add meta data
+    column_to_drop = ['isSg']
+    combined_df_1.columns = ['trf'] + list(combined_df_1.columns[1:])
+    combined_df_1 = combined_df_1.merge(meta_203170, on='trf' , how='left')
+    combined_df_1.drop(column_to_drop, axis=1, inplace=True)
+
+    combined_df_2.columns = ['trf'] + list(combined_df_2.columns[1:])
+    combined_df_2 = combined_df_1.merge(meta_94888, on='trf', how='left')
     # combined_df_2.drop(column_to_drop, axis=1, inplace=True)
 
-    combined_df_1.to_csv(files_location + 'GSE203170_ALS_BoNT_analysis_output.csv')
-    combined_df_2.to_csv(files_location + 'GSE94888_ALS_BoNT_analysis_output.csv')
+    combined_df_1.to_csv(working_dir + 'GSE203170_ALS_BoNT_analysis_output.csv')
+    combined_df_2.to_csv(working_dir + 'GSE94888_ALS_BoNT_analysis_output.csv')
 
     print("All Done")
